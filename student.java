4a610ba67481722cb76a3cdfaeb6e83ed9cbb97f
Ankit Rawat <ankit.rawat3987@gmail.com>
2024-06-25 23:26:59 +0530

    added stundent class

---
+++ student.java
@@ -0,0 +1,11 @@
+public class student {
+    
+    public student(){
+        System.out.println("creating student object");
+
+    }
+    public void sayHello(){
+        System.out.println("i , am student");
+    }
+
+}
