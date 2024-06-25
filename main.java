0e756848803d64faa08161ebe5f6ee7bb121b57c
Ankit Rawat <ankit.rawat3987@gmail.com>
2024-06-25 23:35:43 +0530

    this is creat node file

--- main.java
+++ main.java
@@ -1,13 +1,14 @@
 import java.util.Date;
 
-public class main{
-    public static void main(String[] args){
+public class main {
+    public static void main(String[] args) {
 
         System.out.println("This is java program");
         System.out.println("Output from java program");
         System.out.println(new Date());
         System.out.println("Add one more");
-
+        student student = new student();
+        student.sayHello();
     }
 
 }
---
+++ node-main.js
@@ -0,0 +1,2 @@
+console.log("THIS IS NODE FILE");
+console.log("going to run using jenkins");
